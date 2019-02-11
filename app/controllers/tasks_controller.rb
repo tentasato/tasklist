ebdc113646a328ba9603aa44847d61dc4fcdc608
EC2 Default User <kamedajin@gmail.com>
2019-02-11 09:05:49 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,6 @@ class TasksController < ApplicationController
   end
 
   def show
-     @task = Task.find(params[:id])
   end
 
   def new
@@ -26,12 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-      @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'task は正常に更新されました'
       redirect_to @task
@@ -42,7 +38,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'task は正常に削除されました'
@@ -54,8 +49,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
